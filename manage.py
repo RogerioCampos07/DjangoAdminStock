0746a0daf6c7baad7b0e4245f9c501e3371c5737
Rogerio Campos <rogerio.campos07@gmail.com>
2025-09-24 11:19:57 -0300

    fix: manage.py fixed

--- manage.py
+++ manage.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
@@ -8,7 +7,7 @@ def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
     try:
-        from django.core.management import execute_from_command_line# noqa:PLC0415
+        from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
